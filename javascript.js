28c25e7e9837bc06e376dfb6eeade48198c71167
IllMind26 <bigmoneytalk26@gmail.com>
2022-08-07 02:54:39 -0500

    added first function to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 // create function that generates a random word(Rock, Paper, Scissors) from an array
 
 // create function for the computer to make a random selection from that array
+    function getComputerChoice() {
+    
+    }
 
+    const game = ["rock", "paper", "scissors"];
 // create a prompt for the user to make a selection
 
 // create a function that creates a round, make these rounds repeat 5 times
+
+// within that round function
 
 // Rock beats scissors
 
 // Scissors beats paper
 
 // Paper beats rock
+console.log(game[0]);
