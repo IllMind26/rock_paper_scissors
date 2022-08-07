1acbc10d5a223a5a038258aeb88293ceffe17163
IllMind26 <bigmoneytalk26@gmail.com>
2022-08-07 13:41:16 -0500

    updated javascript.js

--- javascript.js
+++ javascript.js
@@ -3,14 +3,21 @@
 // create function for the computer to make a random selection from that array
     function getComputerChoice() {
     
+        const game = ["Rock", "Paper", "Scissors"];
     }
 
-    const game = ["rock", "paper", "scissors"];
+   
 // create a prompt for the user to make a selection
 
 // create a function that creates a round, make these rounds repeat 5 times
 
 // within that round function
+    function playRound(playerSelection, computerSelection) {
+        // code here
+    }
+
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
 
 // Rock beats scissors
 
