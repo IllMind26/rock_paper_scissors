23d3aa226bc97c08733fbef42779735c51bd0239
IllMind26 <bigmoneytalk26@gmail.com>
2022-08-06 22:36:12 -0500

    Added comments to javascript.js file

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+// create function that generates a random word(Rock, Paper, Scissors) from an array
+
+// create function for the computer to make a random selection from that array
+
+// create a prompt for the user to make a selection
+
+// create a function that creates a round, make these rounds repeat 5 times
+
 // Rock beats scissors
 
 // Scissors beats paper
