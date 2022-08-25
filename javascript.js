17b50797e40dc495ecee1fcdfbc856dee35a98c3
IllMind26 <bigmoneytalk26@gmail.com>
2022-08-25 00:48:55 -0500

    Updated playRound function

--- javascript.js
+++ javascript.js
@@ -21,20 +21,21 @@ const rps = ["rock", "paper", "scissors"];
 
 // create a function to simulate a round
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            console.log("Tie game!!!");
-        } else if (playerSelection === "rock" && computerSelection === "scissors" || 
-        playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors"
-        || playerSelection === "rock" && computerSelection === "paper") {
-            console.log("You Win!!");
-            playerScore++;
-        } else {
-            console.log("You Lose!!");
-            computerScore++;
-        }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } if (playerSelection === 'rock' && computerSelection === 'scissors') {
+      return 'You win!';
+    } if (playerSelection === 'paper' && computerSelection === 'rock') {
+      return 'You win!';
+    } if (playerSelection === 'scissors' && computerSelection === 'paper') {
+      return 'You win';
+    } else {
+      return 'You lose!';
     }
+}
 
+console.log(playRound( playerSelection, computerSelection ));
 // create a function that loops the round 5 times
 
     function game() {
